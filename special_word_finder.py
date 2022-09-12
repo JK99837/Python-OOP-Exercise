6f8f56f4f442b2a7b5ff731641c5168c375e0d40
JK99837 <104741801+JK99837@users.noreply.github.com>
2022-09-12 09:27:12 -0400

    Add files via upload

---
+++ special_word_finder.py
@@ -0,0 +1,7 @@
+from wordfinder import WordFinder
+
+
+class SpecialWordFinder(WordFinder):
+    def __init__(self, path):
+        super().__init__(path)
+        self.words = [word for word in self.words if word.isalpha()]
